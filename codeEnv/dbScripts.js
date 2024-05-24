6d765a13993554b01761d029345473ba59e9a3b2
brad <brad.wils001@gmail.com>
2024-05-24 10:18:48 +1000

    beautified, google sign up funtion implemented, sendmessage
    beatified
    google sign in is implemented, needs to be added onto to actually function in relation to rest of code (bind UUID to profile, welcome/'setup' code, etc)
    
    atm moving onto writing the website, everything funamental works atm (i think)

--- codeEnv/dbScripts.js
+++ codeEnv/dbScripts.js
@@ -5,10 +5,13 @@ var lastDbSnapshot;
 var dbChange = true; //starts as true so the first use calls an update
 
 function rndm(max) {
-  return String(Math.floor(Math.random()*max));
+  return String(Math.floor(Math.random() * max));
 }
 
-async function writeUserData(userID, displayName) {
+async function writeUserData(userID, displayName, googleUUID) {
+  //userID is email
+  //displayName can be (by default) email w/o the @xxx, and can be changed if wanted
+  //UUID is the google sign in ID, and what will be matched when someone signs in with google
   /**
   when writing data:
   - gets the requested userID (unique) and display name (add more parameters to deal with later)
@@ -23,7 +26,7 @@ async function writeUserData(userID, displayName) {
   const reference = ref(db, 'users/' + userID + '/data/');
   //console.log(reference)
 
-  
+
   checkUserValidity(userID).then(async (response) => { //read db, when get a return parse it as 'response'
     if (response == false) {
 
@@ -31,11 +34,11 @@ async function writeUserData(userID, displayName) {
         //db name   db content
         Name: displayName,
         loginKey: rndm(10000)
-        });
-    
+      });
+
       //process.exit;
       //console.log('user data written');
-      
+
     } else {
       return ('user already exists');
     }
@@ -44,8 +47,8 @@ async function writeUserData(userID, displayName) {
 
 //below is currently not in use & has errors (todo)
 
-async function initiateNewConversation(participants) { //NOT TESTED 
-                      //participants needs to be an array
+async function initiateNewConversation(participants) { //works? needs thorough testing though
+  //participants needs to be an array
   if (Array.isArray(participants) == false) {
     return 'notarray';
   }
@@ -54,15 +57,15 @@ async function initiateNewConversation(participants) { //NOT TESTED
   //when this is run, have a switch statement on the other side, which depending on what the returned value is will display error (what kind etc) or success.
 
 
-  
+
 
 
   if (await checkUserValidity(participants) == false) { //make sure all invited people are real users (exist). 
     return 'invaliduser'
   }
-    if (await existingConversationCheck(participants) == true) { //this will be written at a later date
-      console.log('conversation already exists, aborting process');
-    } else { 
+  if (await existingConversationCheck(participants) == true) { //this will be written at a later date
+    console.log('conversation already exists, aborting process');
+  } else {
     //existing convo doesn't exist; users are valid; now we can write the convo.
 
     const db = getDatabase(app);
@@ -81,46 +84,46 @@ async function initiateNewConversation(participants) { //NOT TESTED
 
 
 
-      var reference = ref(db, 'conversations/' + convoID)
+    var reference = ref(db, 'conversations/' + convoID)
 
-      // writeNewConversation(participants) //why is this here? it's just making it be called twice! FUCK!!!! immortalized.
+    // writeNewConversation(participants) //why is this here? it's just making it be called twice! FUCK!!!! immortalized.
 
 
-      for (i = 0; i < participants.length; i++) {
-        console.log('participantsLoop i: ' + i) //for each person, append the new convoID to their conversations list.
-        const db = getDatabase(app);
-                    // users   //   user0           // 
-        await readDB('users/' + participants[i] + '/').then((response) => {
+    for (i = 0; i < participants.length; i++) {
+      console.log('participantsLoop i: ' + i) //for each person, append the new convoID to their conversations list.
+      const db = getDatabase(app);
+      // users   //   user0           // 
+      await readDB('users/' + participants[i] + '/').then((response) => {
 
-          //
-          //read db, when get a return parse it as 'response'
-          if (response !== false) {
+        //
+        //read db, when get a return parse it as 'response'
+        if (response !== false) {
           try {
             //this is being x2'd for some reason. this reason is because it's counting the amount of keys in the object, not the amount of entries in the array.
             //this means that i need to get the amount of entries in an object instead of the amount of keys.
             nextConvoSpace = (Object.keys(response.conversations).length) + 1; //if existing convos, get the amount adn then add for the next convo
           } catch {
             nextConvoSpace = 1;
-          } 
-            var reference = ref(db, 'users/' + participants[i] + '/conversations/');
-            update(reference, {
-              [nextConvoSpace]: convoID, //nextConvoSpace seems to always go up by two; not sure why. otherwise, works! (as far as i've tested it writes in the write spots.)
-              //need to update convo folder too
-            })
-
-
-            //updated the user's data with the new conversation IDs, done! (?)
-            console.log('finished await loop')
-          } else {
-            console.log('badpath')
           }
-        });        //updated conversation ID to be old convos + new
+          var reference = ref(db, 'users/' + participants[i] + '/conversations/');
+          update(reference, {
+            [nextConvoSpace]: convoID, //nextConvoSpace seems to always go up by two; not sure why. otherwise, works! (as far as i've tested it writes in the write spots.)
+            //need to update convo folder too
+          })
 
-        //here, within this conversations 'users' section we're saving an array of users that are in this conversation.
 
-      }
+          //updated the user's data with the new conversation IDs, done! (?)
+          console.log('finished await loop')
+        } else {
+          console.log('badpath')
+        }
+      }); //updated conversation ID to be old convos + new
+
+      //here, within this conversations 'users' section we're saving an array of users that are in this conversation.
+
     }
-    alert('finished')
+  }
+  alert('finished')
 };
 
 
@@ -129,15 +132,15 @@ function readDB(path) { //path should be used like this:
   const db = getDatabase(app); //updates db shit (idrk waht it does)
   const reference = ref(db, path); //sets reference (based on parsed path, etc 'users/id/')
   return get(reference).then((snapshot) => { //get (data referenced) and then parse it as snapshot. the .then makes sure that data is returned BEFORE continuing or else you get a bunch of 'undefined's.
-   if (snapshot.exists()) { //if snapshot does exist
-    console.log('snapshot exists'); //yep
-    lastDbSnapshot = snapshot.val(); //set it to lastDbSnapshot
-    return snapshot.val(); //returns the data as an object to the return function that we're im, and then that return will return data to the function.
+    if (snapshot.exists()) { //if snapshot does exist
+      console.log('snapshot exists'); //yep
+      lastDbSnapshot = snapshot.val(); //set it to lastDbSnapshot
+      return snapshot.val(); //returns the data as an object to the return function that we're im, and then that return will return data to the function.
 
     } else {
       console.log('bad path') //works!
       return false; //return false, bad path
-      
+
     }
   })
 }
@@ -150,18 +153,26 @@ function readDB(path) { //path should be used like this:
 
 
 
-async function sendMessage(conversationID, content, sender, timestamp) { 
+async function sendMessage(conversationID, content, sender, timestamp) {
+  content = String(content) //ensure it's a string
+  
   //what do we want this function to do?
-  //write message data
   const db = getDatabase(app);
-  var reference = ref(db, 'conversations/' + conversationID + '/messages/' + 'unique message ID');
+  var reference = ref(db, 'conversations/' + conversationID + '/messages/' + timestamp);
   await set(reference, {
     Content: content,
     Owner: sender,
-    Time: timestamp
-  });
+  }).catch((error) => {
+    console.error('error while sending message, ' + error)
+  })
   //process.exit;
 }
+
+// example use: sendMessage('1','this is my message', 'user0', Date.now())
+
+
+
+
 // writeMessageData();
 
 // sendMessage(<conversationID>, <content>)
@@ -179,14 +190,14 @@ async function existingConversationCheck(users) { //check notability diagram.
   readDB('users/').then((response) => {
     //find user with least convos
     lowest = 0;
-    for (i=0; i < users.length; i++) {
+    for (i = 0; i < users.length; i++) {
       // for ()
 
 
-        
+
 
     }
-});
+  });
 }
 
 async function getNextAvailableConversationID() { //this works.... perfectly !?
@@ -196,10 +207,10 @@ async function getNextAvailableConversationID() { //this works.... perfectly !?
       if (response !== false) { // if response isn't false (ie is valid)
         console.log('result: ' + JSON.stringify(lastDbSnapshot, null, 2)); //debug log
         console.log('returning (objkeys.len): ' + Object.keys(lastDbSnapshot).length) //debug log
-        return(Number(resolve(Object.keys(lastDbSnapshot).length + 1))); //this returns the following: a number of the amount of entries (+1, as that's the next available convo). this RESOLVES the promise, so then the function that called it can continue to the .then part.
+        return (Number(resolve(Object.keys(lastDbSnapshot).length + 1))); //this returns the following: a number of the amount of entries (+1, as that's the next available convo). this RESOLVES the promise, so then the function that called it can continue to the .then part.
 
       } else {
-        return(0) //if false, there's no convo folder (usually from manual testing, mb)
+        return (0) //if false, there's no convo folder (usually from manual testing, mb)
         throw console.error(error); //the throw will reject the promise. 
       }
     });
@@ -211,9 +222,9 @@ async function checkUserValidity(userID) { //MUST use 'await' checkUserValidity 
   //CURRENT ISSUE: doesn't run loop cirrectly
   var validity = true;
   if (Array.isArray(userID)) { //if userId is an array
-    for (i=0; i < userID.length; i++) { //for each in userID
+    for (i = 0; i < userID.length; i++) { //for each in userID
       //console.log('reading' + userID[i]);
-       await readDB('users/' + userID[i] + '/').then((response) => { //attempt to read that directory
+      await readDB('users/' + userID[i] + '/').then((response) => { //attempt to read that directory
         if (response == false) { //if response is invalid
           validity = false; //is valid
         }
@@ -228,12 +239,12 @@ async function checkUserValidity(userID) { //MUST use 'await' checkUserValidity 
         }
       });
     } else {
-      console.error('bad input:' + typeof(userID))
+      console.error('bad input:' + typeof (userID))
     }
   }
   //console.log('returned checkUserValidity');
   //console.log(validity);
-  return validity;//returns true or false depending on whether or not shit is good
+  return validity; //returns true or false depending on whether or not shit is good
 } //works!
 
 
@@ -261,73 +272,85 @@ async function writecustompath(path, dataName, data) {
 
     // participants: 'egawhdasd'
 
- 
-    });
+
+  });
 
   //process.exit;
   //console.log('user data written');
-  }
+}
 
-  async function writeNewConversation(participants, id) { //id is undefined; why?
-    console.log(participants + '\n' + id)
-    //participants needs to be array, id needs to be string
-    //for convo we need:
-    // (d = data, f = folder, #f = made as needed
-    //    overarching folder with ID
-    //        [d] participants
-    //        [f] messages
-                //FOR EACH MESSAGE (address in send msg func)
-    //          [#f] messageID
-    //            [d] content
-    //            [d] owner
-    //            [d] timestamp
-    const db = getDatabase(app);
-    var reference = ref(db, 'conversations/' + id + '/');
-    console.log('setting ref')
-    await set(reference, {
-      participants: participants, //nextConvoSpace seems to always go up by two; not sure why. otherwise, works! (as far as i've tested it writes in the write spots.)
-      //need to update convo folder too
-    })
-  }
-
-
-
+async function writeNewConversation(participants, id) { //id is undefined; why?
+  console.log(participants + '\n' + id)
+  //participants needs to be array, id needs to be string
+  //for convo we need:
+  // (d = data, f = folder, #f = made as needed
+  //    overarching folder with ID
+  //        [d] participants
+  //        [f] messages
+  //FOR EACH MESSAGE (address in send msg func)
+  //          [#f] messageID
+  //            [d] content
+  //            [d] owner
+  //            [d] timestamp
+  const db = getDatabase(app);
+  var reference = ref(db, 'conversations/' + id + '/');
+  // conversations/1/f
+  console.log('setting ref')
+  await set(reference, {
+    participants: participants, //nextConvoSpace seems to always go up by two; not sure why. otherwise, works! (as far as i've tested it writes in the write spots.)
+    //need to update convo folder too
+  })
+}
 
 
-  async function setTempToData(path) //request data from database using readDB(), and set it to a data named temp
-  {
-    await readDB(path + '/').then((response) => { //read db, when get a return parse it as 'response'
-      if (response !== false) {
-        temp = response;
-        //console.log('temp set to data');
-      }
-    });
-  }
 
-  function setup() {
-    auth.languageCode = 'en';
-  }
 
-  //FUNCTIONS FOR BELOW
-  var signupCredential;
-  var signupToken
-  var signupUser;
-  var errorcode;
-  var errorMessage;
-  var email;
-  var credential;
-   //from https://firebase.google.com/docs/auth/web/google-signin?authuser=1
-  //  signInWithPopup(???, GoogleAuthProvider)
-async function a() {
+
+async function setTempToData(path) //request data from database using readDB(), and set it to a data named temp
+{
+  await readDB(path + '/').then((response) => { //read db, when get a return parse it as 'response'
+    if (response !== false) {
+      temp = response;
+      //console.log('temp set to data');
+    }
+  });
+}
+
+
+//FUNCTIONS FOR BELOW
+var signupCredential;
+var signupToken
+var signupUser;
+var errorMessage;
+var email;
+var UUID;
+//from https://firebase.google.com/docs/auth/web/google-signin?authuser=1
+//  signInWithPopup(???, GoogleAuthProvider)
+
+
+
+async function userSignInPopupFunction() {
   signInWithPopup(Auth, new GoogleAuthProvider())
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       signupCredential = GoogleAuthProvider.credentialFromResult(result);
-      signupToken = credential.accessToken;
+      signupToken = signupCredential.accessToken;
       // The signed-in user info.
-      signupUser = result.user;
-      console.log('user: ' + user.Stringify + '\n token: ' + token + '\n credential: ' + credential.Stringify)
-      // IdP data available using getAdditionalUserInfo(result)
+      signupUser = result.user; //this has most of the useable data
+      loginEmail = result.user.email;
+      UUID = result.user.uid;
+      console.log('date now: ' + Date.now() + '\n signupDate: ' + signupUser.metadata.createdAt);
+      //form testing; this createdAt data is about 30-40s behind from Date.now().
+      //if account created within 60s ( + buffer ) of current time, it's a new account.
+      //so thats 100000ms
+      if (Date.now() - signupUser.metadata.createdAt < 100000) { //if the account's creation time is less than 100 (60-70 realtime) seconds to the current time.
+        //new user
+        onboard = true;
+      } else {
+        //existing user
+      }
+      welcome(onboard)
+
       // ...
     }).catch((error) => {
       console.log(error)
@@ -341,3 +364,14 @@ async function a() {
       // ...
     });
 }
+
+
+function welcome(newUser) {
+  if (newUser == true) {
+    //create new db entry for user
+    //redirect to onboarding screen.
+  } else {
+    //popup successful login, and display sign out button on HTML
+  }
+}
+
